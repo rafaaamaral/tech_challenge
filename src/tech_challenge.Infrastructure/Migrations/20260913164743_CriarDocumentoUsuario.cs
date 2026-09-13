595b77e16bcd2935a6d7cfb03bbf9202a72f2953
Rafael Amaral <rafael.amaral121@hotmail.com>
2026-09-13 13:50:08 -0300

    feat: inclusao migration para executar a pipeline

---
+++ src/tech_challenge.Infrastructure/Migrations/20260913164743_CriarDocumentoUsuario.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tech_challenge.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarDocumentoUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Documento",
+                table: "Usuario",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Documento",
+                table: "Usuario");
+        }
+    }
+}
